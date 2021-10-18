e97dabc25508a4affdbacb642249d9242921f4d8
DeXtreme <dbakolbire@gmail.com>
2021-10-18 23:24:45 +0000

    Update api and db base images

--- Backend/api/api/settings.py
+++ Backend/api/api/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default' : {
-        "ENGINE" : "django.contrib.gis.db.backends.postgis",
+        "ENGINE" : "django.db.backends.postgresql_psycopg2",
         "NAME" : os.getenv("POSTGRES_DB"),
         "USER" : os.getenv("POSTGRES_USER"),
         "PASSWORD" : os.getenv("POSTGRES_PASSWORD"),
