d78175a04efc437997b546d86cfed1decda3ed36
DeXtreme <dbakolbire@gmail.com>
2021-10-19 23:45:56 +0000

    Add article model serializer

---
+++ Backend/api/v1/articles/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Article
+
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = "__all__"
+        read_only = "__all__"
