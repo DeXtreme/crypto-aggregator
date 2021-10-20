f4f7c1d8a4639a0448fc28a583e05ef71dc4e7b2
DeXtreme <dbakolbire@gmail.com>
2021-10-20 23:41:28 +0000

    Add app_name to articles module

--- Backend/api/v1/articles/urls.py
+++ Backend/api/v1/articles/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from .views import ArticleViewSet
 
+app_name = "articles"
+
 router = DefaultRouter()
 router.register(r"", ArticleViewSet, basename="articles")
-
-urlpatterns = []
-urlpatterns += router.urls
+urlpatterns = router.urls
