b47d288117563ddeca2201398eaa3d40582f6bc9
DeXtreme <dbakolbire@gmail.com>
2021-10-22 13:42:18 +0000

    Create and register account model

--- Backend/api/v1/account/admin.py
+++ Backend/api/v1/account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+@admin.register(Account)
+class AccountModelAdmin(admin.ModelAdmin):
+    pass
--- Backend/api/v1/account/models.py
+++ Backend/api/v1/account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    """Model to store account information
+    
+    Attributes
+    ----------
+    user : User
+        The user model associated to the 
+        account
+    name : str
+        The account username
+    photourl: str
+        A link to the account profile picture
+    """
+
+    user = models.OneToOneField(User, models.CASCADE, related_name="account")
+    name = models.CharField(max_length=50)
+    photourl = models.URLField(null=True)
+    
+    def __str__(self):
+        return self.username
