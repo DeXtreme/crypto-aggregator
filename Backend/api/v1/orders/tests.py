dc07ec6c9715d9cd433830a94915cb6802e97408
DeXtreme <dbakolbire@gmail.com>
2021-10-24 23:34:53 +0000

    Add Region Serializer test

--- Backend/api/v1/orders/tests.py
+++ Backend/api/v1/orders/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from rest_framework import serializers
+from rest_framework.test import APITestCase
+from .models import Region, Location
+from .serializers import RegionSerializer
+class RegionsTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        region = Region.objects.create(name="Test region")
+        for i in range(5):
+            location = Location.objects.create(region=region,name=f"Test Location {i}")
 
-# Create your tests here.
+    def test_region_serializer(self):
+        regions = Region.objects.all()
+        serializer = RegionSerializer(regions, many=True)
+        data = serializer.data
+        print(data)
+        self.assertIn("name", data[0])
+        self.assertIn("locations", data[0])
+        self.assertEqual(len(data[0]["locations"]), 5)
+        self.assertIn("name",data[0]["locations"][0])
+
+
+        
