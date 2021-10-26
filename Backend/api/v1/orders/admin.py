50c4057d6af5c646bdac925477e2508a39433ad0
DeXtreme <dbakolbire@gmail.com>
2021-10-26 22:45:59 +0000

    Register order,region,location models

--- Backend/api/v1/orders/admin.py
+++ Backend/api/v1/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from rest_framework.fields import ImageField
+from .models import Order, Region, Location
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
