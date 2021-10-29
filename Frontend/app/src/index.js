5e782a42be84722eae30c18bcec122268f927bef
DeXtreme <dbakolbire@gmail.com>
2021-10-29 23:33:14 +0000

    Add store provider to app root

--- Frontend/app/src/index.js
+++ Frontend/app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/store';
 import reportWebVitals from './reportWebVitals';
-
+import './index.css';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+,
   document.getElementById('root')
 );
 
