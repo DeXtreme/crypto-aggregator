889334a45e020c9b1f96471fb82cb29567abf289
DeXtreme <dbakolbire@gmail.com>
2021-10-30 23:30:42 +0000

    Refactor login class to loginbtn

--- Frontend/app/src/components/navbar/navbar.js
+++ Frontend/app/src/components/navbar/navbar.js
@@ -1,17 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
-import { selectView, removeAccount } from '../../store/actions';
+import { selectView, removeAccount, showLogin } from '../../store/actions';
 import "./navbar.css";
 
-function Navbar({showTabs}){
+function Navbar(){
     let account = useSelector(state => state.account);
+    let view = useSelector(state => state.view)
     let dispatch = useDispatch()
-    const {isDropShow, showDrop } = useState(false);
+    const [isDropShow, showDrop ] = useState(false);
 
     const selectNews = () => dispatch(selectView("news"));
     const selectPrices = () => dispatch(selectView("prices"));
     const selectOrders = () => dispatch(selectView("orders"));
     const logout = () => dispatch(removeAccount());
+    const login = () => dispatch(showLogin(true));
     return (
         <div className="navbar">
             <div className="primary">
@@ -20,7 +22,7 @@ function Navbar({showTabs}){
                 </div>
                 <div className="account">
                     {(account==null) ? 
-                    <button className="login">Login</button>
+                    <button className="loginbtn" onClick={login}>Login</button>
                     :
                     <div className="profile">
                         <img />
@@ -35,9 +37,9 @@ function Navbar({showTabs}){
             </div>
             <div className="secondary">
                 <div className="tabs">
-                    <button onClick={selectOrders}><i class="fas fa-shopping-cart"></i></button>
-                    <button onClick={selectNews}><i class="fas fa-newspaper"></i> </button>
-                    <button onClick={selectPrices}><i class="fas fa-dollar-sign"></i></button>
+                    <button className={(view==="orders") && "active"} onClick={selectOrders}><i class="fas fa-shopping-cart"></i></button>
+                    <button className={(view==="news") && "active"} onClick={selectNews}><i class="fas fa-newspaper"></i> </button>
+                    <button className={(view==="prices") && "active"} onClick={selectPrices}><i class="fas fa-dollar-sign"></i></button>
                 </div>
             </div>
         </div>
