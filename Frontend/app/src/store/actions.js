dd2b485b2d44dc21df385d436583ff859e93cab9
DeXtreme <dbakolbire@gmail.com>
2021-10-30 23:29:53 +0000

    Add showLogin action

--- Frontend/app/src/store/actions.js
+++ Frontend/app/src/store/actions.js
@@ -1,7 +1,9 @@
 export const ADD_ACCOUNT = "account/add";
 export const REMOVE_ACCOUNT = "account/remove";
 export const SELECT_VIEW = "view/select";
+export const SHOW_LOGIN = "login/show"
 
 export const addAccount = (account) => ({type:ADD_ACCOUNT, payload:account,});
 export const removeAccount = (account) => ({type:ADD_ACCOUNT});
 export const selectView = (view) => ({type: SELECT_VIEW, payload: view})
+export const showLogin = (show) => ({type: SHOW_LOGIN, payload: show})
--- Frontend/app/src/store/reducer.js
+++ Frontend/app/src/store/reducer.js
@@ -3,21 +3,25 @@ import * as ACTIONS from './actions';
 let initial_state = {
     account: null,
     view: "news",
+    login: false
 }
 
 function reducer(state = initial_state, action){
     switch(action.type){
         case ACTIONS.ADD_ACCOUNT:
             let account = {
-                name : action.payload["name"],
-                photourl : action.payload["photourl"],
-                token : action.payload["token"]
+                name : action.payload.name,
+                photourl : action.payload.photourl,
+                token : action.payload.token
             }
             return {...state, account: account}
         case ACTIONS.REMOVE_ACCOUNT:
             return {...state, account: null};
         case ACTIONS.SELECT_VIEW:
             return {...state, view:action.payload}
+        case ACTIONS.SHOW_LOGIN:
+            let show = !state.account && action.payload;
+            return {...state, login: show};
         default:
             return state;
     }
