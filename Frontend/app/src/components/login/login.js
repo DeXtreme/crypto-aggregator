baa0230b43219197b06cc484758178be3216a7a4
DeXtreme <dbakolbire@gmail.com>
2021-10-31 13:42:46 +0000

    Add social login funtions

--- Frontend/app/src/components/login/login.js
+++ Frontend/app/src/components/login/login.js
@@ -1,5 +1,11 @@
+import { getAuth, 
+        signInWithPopup,
+        GoogleAuthProvider,
+        TwitterAuthProvider,
+        FacebookAuthProvider } from "firebase/auth";
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import app from "../../firebase";
 import { showLogin } from '../../store/actions'; 
 import './login.css';
 
@@ -11,7 +17,36 @@ function Login(){
         return () => document.body.classList.remove("overflow-hide");
     },[]);
 
-    const doLogin = () => {};
+    const gmailLogin = () => {
+        const provider = new GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
+        doLogin(provider)
+    }
+
+    const twitterLogin = () => {
+        const provider = new TwitterAuthProvider();
+        doLogin(provider)
+    }
+
+    const facebookLogin = () => {
+        const provider = new FacebookAuthProvider();
+        doLogin(provider)
+    }
+
+    const doLogin = (provider) => {    
+        const auth = getAuth(app);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+                console.log(user)
+            }).catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage)
+            });
+    }
+
     const hideLogin = () => dispatch(showLogin(false));
 
     return (
@@ -22,9 +57,9 @@ function Login(){
                     <p>CEDIX</p>
                 </div>
                 <p>Log in with:</p>
-                <button id="gmail"><i class="fab fa-google"></i><span>Gmail</span></button>
-                <button id="twitter"><i class="fab fa-twitter"></i><span>Twitter</span></button>
-                <button id="facebook"><i class="fab fa-facebook-f"></i><span>Facebook</span></button>
+                <button id="gmail" onClick={gmailLogin}><i class="fab fa-google"></i><span>Gmail</span></button>
+                <button id="twitter" onClick={twitterLogin}><i class="fab fa-twitter"></i><span>Twitter</span></button>
+                <button id="facebook" onClick={facebookLogin}><i class="fab fa-facebook-f"></i><span>Facebook</span></button>
             </div>
         </div>
     );
