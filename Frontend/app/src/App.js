b0e859d993b0b3d913d4e749c643913415d7cdf4
DeXtreme <dbakolbire@gmail.com>
2021-11-05 22:12:50 +0000

    Add slider to app

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,8 +1,10 @@
 import { useSelector } from 'react-redux';
 import Navbar from "./components/navbar/navbar";
 import Login from "./components/login/login";
+import Slider from './components/slider/slider';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   let showLogin = useSelector(state => state.login);
@@ -10,20 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       {showLogin && <Login />} 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider />
     </div>
   );
 }
