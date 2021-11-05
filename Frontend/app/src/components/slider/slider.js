0da8377f7d019582b6215416838baf8e25f41255
DeXtreme <dbakolbire@gmail.com>
2021-11-05 22:12:26 +0000

    Add slider component

---
+++ Frontend/app/src/components/slider/slider.js
@@ -0,0 +1,25 @@
+import { useSelector,useDispatch } from 'react-redux';
+import News from '../../pages/news/news';
+import './slider.css';
+
+function Slider(props){
+    let view = useSelector(state => state.view);
+    console.log(view);
+    return (
+        <div class="slider">
+            <div className={`panes ${(view==="orders") ? "left" : (view==="news") ? "middle" : "right"}`}>
+                <div className="side">
+                    
+                </div>
+                <div className="main">
+                    <News />
+                </div>
+                <div className="side">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
