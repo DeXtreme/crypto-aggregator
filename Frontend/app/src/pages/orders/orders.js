0b7817420fbb4e2346a83f1efb23cd964a57985c
DeXtreme <dbakolbire@gmail.com>
2021-11-11 15:15:11 +0000

    Add add order modal

--- Frontend/app/src/pages/orders/orders.js
+++ Frontend/app/src/pages/orders/orders.js
@@ -3,10 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import Order from '../../components/order/order';
 import { API_HOST } from '../../config';
 import { setRegions } from '../../store/actions';
+import { showLogin } from '../../store/actions';
 import "./orders.css";
 
 function Orders(){
     let [ orderView, setOrderView ] = useState("orders");
+    let [ isShowAddOrder, setShowAddOrder ] = useState(false);
+    let account = useSelector(state => state.account);
     let orders = useSelector(state => state.orders);
     let regions = useSelector(state => state.regions);
     let myOrders = useSelector(state => state.myOrders);
@@ -14,7 +17,14 @@ function Orders(){
 
     let locations = regions.reduce((prev,region)=>prev.concat(region.locations),[])
                            .sort((a,b)=>a.name>b.name)
-    console.log(locations);
+
+    const showAddOrder = (show)=>{
+        //if(account==null){
+        //    dispatch(showLogin(true));
+        //}else{
+            setShowAddOrder(true);
+        //}
+    }
 
     useEffect(()=>{
         let url = `${API_HOST}v1/orders/locations`;
@@ -28,15 +38,15 @@ function Orders(){
 
     return (
         <div className="orders">
-            <div className="tabs">
+            {account && <div className="tabs">
                 <button className={(orderView=="orders") ? "active": ""}
                         onClick={()=>setOrderView("orders")}>Orders</button>
                 <button className={(orderView=="myorders") ? "active": ""}
                         onClick={()=>setOrderView("myorders")}>My Orders</button>
-            </div>
+            </div>}
 
             {(orderView=="orders") ? <div className="all">
-                <button>Post Order</button>
+                <button onClick={showAddOrder}>Post Order</button>
                 <div className="filters">
                     <select>
                         <option>All</option>
@@ -79,6 +89,32 @@ function Orders(){
         :<div>
          myOrders
          </div>}
+        {isShowAddOrder && <div className="add">
+            <div id="background" />
+            <form>
+                <h1>Add Order</h1>
+                <div>        
+                <select>
+                    <option value="B">Buying</option>
+                    <option value="S">Selling</option>
+                </select>
+                <select>
+                    <option value="BTC">BTC</option>
+                    <option value="ETH">ETH</option>
+                    <option value="USDT">USDT</option>
+                    <option value="USDC">USDC</option>
+                    <option value="BUSD">BUSD</option>
+                </select>
+                </div>          
+                <input placeholder="Price" inputMode="decimal" required />        
+                <input placeholder="Display Name" inputMode="text" required />
+                <input placeholder="Display Contact" inputMode="text" required />
+                <select>
+                    {locations.map(location=><option key={location.id} value={location.id}>{location.name}</option>)}
+                </select>
+                <button>Submit</button>                  
+            </form>
+        </div>}
     </div>
     );
 }
