bfd7b2a3d3750ba75fc67e9266e0f6e521b4c3bb
DeXtreme <dbakolbire@gmail.com>
2021-11-13 22:56:18 +0000

    Fix add order view

--- Backend/api/v1/orders/views.py
+++ Backend/api/v1/orders/views.py
@@ -5,13 +5,14 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Order, Region
-from .serializers import OrderSerializer, RegionSerializer
+from .serializers import OrderSerializer, RegionSerializer, NewOrderSerializer
 from .permissions import IsPoster
 
 class OrdersViewSet(ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    serializer_class = OrderSerializer
     queryset = Order.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields =  {"account" : ["exact"],
@@ -25,6 +26,21 @@ class OrdersViewSet(ModelViewSet):
         if self.request.method not in permissions.SAFE_METHODS:
             self.permission_classes = [IsAuthenticated, IsPoster]
         return [permission() for permission in self.permission_classes]
+    
+    def get_serializer_class(self):
+        if self.request.method not in permissions.SAFE_METHODS:
+            return NewOrderSerializer
+        else:
+            return OrderSerializer
+    
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        account = user.account
+        serializer = self.get_serializer(data={**request.data,"account":account.pk})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class RegionView(ListAPIView):
     queryset = Region.objects.all()
