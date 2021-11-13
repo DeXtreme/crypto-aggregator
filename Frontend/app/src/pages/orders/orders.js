8872f2c4db28fbfdb5a23bf6ec37cb937e3f352a
DeXtreme <dbakolbire@gmail.com>
2021-11-13 22:58:08 +0000

    Add loadOrders and loadMyOrders

--- Frontend/app/src/pages/orders/orders.js
+++ Frontend/app/src/pages/orders/orders.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Order from '../../components/order/order';
 import { API_HOST } from '../../config';
-import { setRegions } from '../../store/actions';
+import { setRegions, setOrders, setMyOrders } from '../../store/actions';
 import { showLogin } from '../../store/actions';
 import "./orders.css";
 
@@ -19,6 +19,7 @@ function Orders(){
         location: ""
     })
     let [ error, setError ] = useState(null);
+    let [ loading, setLoading ] = useState(false);
 
     let account = useSelector(state => state.account);
     let orders = useSelector(state => state.orders);
@@ -34,7 +35,9 @@ function Orders(){
                 setShowAddOrder(true);
             }
         }else{
-            setShowAddOrder(false);
+            if(!loading){
+                setShowAddOrder(false);
+            }
         }
     }
 
@@ -48,6 +51,8 @@ function Orders(){
 
     const addOrder = (e) =>{
         e.preventDefault();
+        setError(null);
+        setLoading(true);
         let url = `${API_HOST}v1/orders/`;
         fetch(url, {method:"POST", 
                     headers:{'Authorization': `Token ${account.token}`,
@@ -55,11 +60,33 @@ function Orders(){
                     body:JSON.stringify(newOrder)})
               .then(response => response.json())
               .then(json=>{
-                  console.log(json)
+                  loadOrders();
+                  setLoading(false);
+                  showAddOrder(false);
               }).catch(error => {
                   console.log(error.message);
                   setError(error.message)
-              })
+              }).finally((e)=> setLoading(false))
+    }
+
+    const loadOrders = ()=>{
+        let url = `${API_HOST}v1/orders/`;
+        fetch(url).then(response => response.json())
+        .then((json)=>{
+            dispatch(setOrders(json));         
+        }).catch(error=>console.log(error.message))
+    }
+
+    const loadMyOrders = () =>{
+        if(account){
+            let url = `${API_HOST}v1/orders/?account=${account.id}`;
+            fetch(url).then(response => response.json())
+            .then((json)=>{
+                console.log("myorders")
+                console.log(json);
+                dispatch(setMyOrders(json));         
+            }).catch(error=>console.log(error.message))
+        }
     }
 
     useEffect(()=>{
@@ -76,8 +103,14 @@ function Orders(){
                 return order;
             })
         }).catch(error=>console.log(error.message))
+
+        loadOrders();
+        loadMyOrders();
     },[]);
 
+    useEffect(()=>{
+        loadMyOrders();
+    },[account])
 
     return (
         <div className="orders">
@@ -119,14 +152,7 @@ function Orders(){
                     <input placeholder="Max price" />
                 </div>
             <div className="list">
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
-                <Order />
+                {orders.map(order => <Order order={order} key={order.id} />)}
             </div>
         </div>
         :<div>
@@ -134,7 +160,7 @@ function Orders(){
          </div>}
         {isShowAddOrder && <div className="add">
             <div onClick={()=>showAddOrder(false)} id="background" />
-            <form onSubmit={addOrder}>
+            {(!loading) ? <form onSubmit={addOrder}>
                 <h1>Add Order</h1>
                 { error && <p id="error">{error}</p>}
                 <div>        
@@ -157,7 +183,13 @@ function Orders(){
                     {locations.map((location,i)=><option key={location.id} value={location.id} >{location.name}</option>)}
                 </select>
                 <button type="submit">Submit</button>                  
-            </form>
+            </form> :
+            <div>
+                <div class="loadingio-spinner-eclipse-rxkb3szgim"><div class="ldio-vh4jl67dfj8">
+                <div></div>
+                </div></div>
+            </div>
+            }
         </div>}
     </div>
     );
