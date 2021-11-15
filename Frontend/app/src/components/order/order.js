48f39057028ac39698f412901a74f421d0b3fe8d
DeXtreme <dbakolbire@gmail.com>
2021-11-15 22:28:02 +0000

    Add menu to order

--- Frontend/app/src/components/order/order.js
+++ Frontend/app/src/components/order/order.js
@@ -1,21 +1,46 @@
+import { useState } from 'react';
 import './order.css';
 
-function Order({order}){
+function Order({order,editable,handleEdit,handleDelete}){
     const types = {B:"Buying",S:"Selling"}
+
+    let [ isShowMenu, setShowMenu ] = useState(false);
+
+    const showMenu = () =>{
+        setShowMenu(prev=>!prev);
+    }
+
     return (
         <div className="order">
             <hr />
-            <div className="type-price">
-                <p className={(order.type=="B") ? "buy" : "sell"}>{types[order.type]} <span>{order.coin}</span></p>
-                <p className="price"><span>GHc</span>{order.price}</p>
-            </div>
-            
-            <div className="details">
-                <div>
-                    <p className="by">{order.by}</p>
-                    <p className="location">{`${order.location.region}, ${order.location.name}`}</p>
+            <div className="content">
+                <div className="card">
+                    <div className="type-price">
+                        <p className={(order.type=="B") ? "buy" : "sell"}>{types[order.type]} <span>{order.coin}</span></p>
+                        <p className="price"><span>GHc</span>{order.price}</p>
+                    </div>
+                    
+                    <div className="details">
+                        <div>
+                            <p className="by">{order.by}</p>
+                            <p className="location">{`${order.location.region}, ${order.location.name}`}</p>
+                        </div>
+                        <p className="contact">{order.contact}</p>
+                    </div>
                 </div>
-                <p className="contact">{order.contact}</p>
+                { editable &&
+                <div className="menu">
+                    <button className="button" onClick={showMenu}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                    <div className={`dropdown ${(isShowMenu) ? "show" : ""}`}>
+                        <button onClick={()=>handleEdit(order.id)}>Edit</button>
+                        <button onClick={()=>handleDelete(order.id)}>Delete</button>
+                    </div>
+                </div>
+                }
             </div>
             
         </div>
