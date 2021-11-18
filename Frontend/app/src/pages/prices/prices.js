e51c9e8695aff8d37248419560c49956c724ce21
DeXtreme <dbakolbire@gmail.com>
2021-11-18 21:48:36 +0000

    Add Price to prices page

--- Frontend/app/src/pages/prices/prices.js
+++ Frontend/app/src/pages/prices/prices.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setPrices } from '../../store/actions';
+import Price from '../../components/price/price';
 import './prices.css';
 
 function Prices(){
@@ -9,7 +10,7 @@ function Prices(){
     let dispatch = useDispatch();
 
     const loadPrices = ()=>{
-        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false")
+        fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
         .then(response => response.json())
         .then(json=>{
             console.log(json[0])
@@ -25,7 +26,9 @@ function Prices(){
     },[]);
 
     return(
-        <div>Prices</div>
+        <div id="prices">
+            {Object.keys(prices).map(id=><Price price={prices[id]} key={id}/>)}
+        </div>
     );
 }
 
