651c88c9d7a98c29287deb261104a7113879cad6
DeXtreme <dbakolbire@gmail.com>
2021-12-19 21:47:20 +0000

    Remove article description

--- Frontend/app/src/components/article/article.js
+++ Frontend/app/src/components/article/article.js
@@ -7,7 +7,7 @@ function Article({article}){
                 {article.image && <img src={article.image} loading="lazy"/>}
                 <div className="details">
                     <h2 className="title">{article.title}</h2>
-                    <p className="description">{article.description}</p>
+                    <p className="description">{/*article.description*/}</p>
                     <p className="source">By {article.source}</p>
                 </div>
             </a>
